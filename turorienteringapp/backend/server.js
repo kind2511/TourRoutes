a650d1e99a8c9da509cd5d914081e016189cd001
kind2511 <chrisa2511@gmail.com>
2023-09-21 22:24:00 +0200

    fixed a comment

--- turorienteringapp/backend/server.js
+++ turorienteringapp/backend/server.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
-//specifies app
+//specifies path to config.env file
 dotenv.config({ path: "./config.env" });
 
 const app = require("./app");
