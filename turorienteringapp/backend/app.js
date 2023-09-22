2f24dccb5b17f3ef6b0baa50e85392ed724948ee
kind2511 <chrisa2511@gmail.com>
2023-09-22 13:32:48 +0200

    added some comments

--- turorienteringapp/backend/app.js
+++ turorienteringapp/backend/app.js
@@ -2,6 +2,9 @@ const express = require("express");
 
 const app = express();
 
+// Middleware (Middle of request and response)
+
+// This populates the body(req.body) with data that you send to it. 
 app.use(express.json());
 
 // Creating a simple rout.
