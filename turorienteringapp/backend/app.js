255596f06402212dee9206fb0be88ab6929b4420
kind2511 <chrisa2511@gmail.com>
2023-09-25 18:13:21 +0200

    Created some simple routes to understand how express workd

--- turorienteringapp/backend/app.js
+++ turorienteringapp/backend/app.js
@@ -3,14 +3,24 @@ const express = require("express");
 const app = express();
 
 // Middleware (Middle of request and response)
-
-// This populates the body(req.body) with data that you send to it. 
+// This populates the body(req.body) with data that you send to it.
 app.use(express.json());
 
-// Creating a simple rout.
-app.get("/", (req, res) => {
-  res.status(200).json({ message: "First Express Route" });
-});
+//---------------------------------------------------------------------------
+
+const getTestMessage = (req, res) => {
+  res.status(200).json({ message: "TurRute" });
+};
+
+const getAllTestmessages = (req, res) => {
+  res.status(200).json({ message: "Alle Turruter" });
+};
+
+// Creating simple routes for testing
+app.get("/turruter", getTestMessage);
+app.get("/alleturruter", getAllTestmessages);
+
+
+//----------------------------------------------------------------------------
 
 module.exports = app;
-
