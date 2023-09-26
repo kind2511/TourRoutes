7fdac1835f740ef1c46f5b2e2cc026f179274ad4
ahmadmm <ahmadmm@stud.ntnu.no>
2023-09-26 12:34:06 +0200

    added home page components and styled bottom line

--- turorienteringapp/frontend/src/App.js
+++ turorienteringapp/frontend/src/App.js
@@ -1,38 +1,35 @@
+// src/App.js
+
 import React from 'react';
 // Import necessary components from the React Router library for handling navigation
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// Import the SignIn, Register pages, and NavigationBar
+// Import the SignIn, Register pages, HomePage, and NavigationBar
 import SignIn from './pages/SignIn';
 import Register from './pages/Register';
 import NavigationBar from './components/NavigationBar';
+import HomePage from './components/HomePage';  // importing the HomePage component
 
 // Main App function component
 function App() {
-  return (
-    <div className="App">
-      
-      <Router>
-        
-        <NavigationBar /> {/* display the navigation bar on every page */}
-        
-        <Routes>
-          
-          {/* Default route that redirects to the "/login" page */}
-          {/* Commenting out the line below to prevent automatic redirection to the login page */}
-          {/* <Route path="/" element={<Navigate to="/login" replace />} /> */}
-          
-          {/* Route for the login page */}
-          <Route path="/login" element={<SignIn />} />
-          
-          {/* Route for the registration page */}
-          <Route path="/register" element={<Register />} />
-          
-          {/* Add other routes here as needed */}
-          
-        </Routes>
-      </Router>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Router>
+                <NavigationBar /> {/* display the navigation bar on every page */}
+                <Routes>
+                    {/* Route for the HomePage */}
+                    <Route path="/" element={<HomePage />} />
+                    
+                    {/* Route for the login page */}
+                    <Route path="/login" element={<SignIn />} />
+                    
+                    {/* Route for the registration page */}
+                    <Route path="/register" element={<Register />} />
+                    
+                    {/* Add other routes here as needed */}
+                </Routes>
+            </Router>
+        </div>
+    );
 }
 
 /* Export the App component to be used elsewhere in the project*/
---
+++ turorienteringapp/frontend/src/components/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './HomePage.css';
+
+function HomePage() {
+    return (
+        <div className="home-page">
+            <p>Explore the map and find your route!</p>
+            <div className="bottom-bar"></div>
+        </div>
+    );
+}
+
+export default HomePage;
