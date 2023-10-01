3349387c9c78515ed45fc9d1f8b3b596faafce4e
ahmadmm <ahmadmm@stud.ntnu.no>
2023-10-01 17:58:10 +0200

    Updated padding for register and sign-in containers and reduced top margin. Removed redundant CSS.

--- turorienteringapp/frontend/src/pages/Register.js
+++ turorienteringapp/frontend/src/pages/Register.js
@@ -11,11 +11,11 @@ function Register() {
     password: '',
     confirmPassword: ''
   });
-  
+
   // Hook from 'react-router-dom' to programmatically navigate between routes
   const navigate = useNavigate();
 
-  // Handler to update state when input fields change
+   // Handler to update state when input fields change
   const handleInputChange = (event) => {
     const { name, value } = event.target;
     setFormData(prevState => ({ ...prevState, [name]: value }));
@@ -30,32 +30,14 @@ function Register() {
       console.log("Passwords don't match!");
       return;
     }
-
-    // This is where you'd typically make a backend call to register the user
-    // It's commented out for now, but serves as a placeholder for that functionality
-    /*
-    try {
-        const response = await axios.post('/api/register', formData);
-        if (response.data.success) {
-            console.log('Registration successful!');
-            navigate('/login');  // Navigate to login after successful registration
-        } else {
-            console.log('Registration failed!:', response.data.message);
-        }
-    } catch (error) {
-        console.log('An error occurred while registering:', error);
-    }
-    */
-
+    
     // Debugging: log the form data
     console.log(formData);
-  }
-
-  // Handler to navigate to the login page
+  }// Handler to navigate to the login page
   const handleLoginClick = () => {
     navigate('/login');
   }
-
+ 
   // Render the registration form
   return (
     <div className="register-background">
@@ -64,12 +46,21 @@ function Register() {
         <span className="nav-logo">TurRuter</span>
         
         <form onSubmit={handleSubmit}>
-          <label htmlFor="name">Name</label>
+          <label htmlFor="firstName">First Name</label>
           <input 
             type="text"
-            name="name"
-            id="name"
-            value={formData.name}
+            name="firstName"
+            id="firstName"
+            value={formData.firstName}
+            onChange={handleInputChange}
+          />
+
+          <label htmlFor="lastName">Last Name</label>
+          <input 
+            type="text"
+            name="lastName"
+            id="lastName"
+            value={formData.lastName}
             onChange={handleInputChange}
           />
 
