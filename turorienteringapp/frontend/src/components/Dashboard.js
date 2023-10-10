34d0f0dc68eb914355e0c50bedcff25b33da02fd
ahmadmm <ahmadmm@stud.ntnu.no>
2023-10-10 15:37:28 +0200

    Updated Dashboard UI and improved logout functionality

--- turorienteringapp/frontend/src/components/Dashboard.js
+++ turorienteringapp/frontend/src/components/Dashboard.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom'; /* Importing useNavigate*/
+import { useNavigate } from 'react-router-dom'; /* Importing useNavigate */
 import './Dashboard.css';
 
 const Dashboard = () => {
     const navigate = useNavigate();
-    {/* Log out and redirect ot login page after */}
+    {/* Log out and redirect to login page after */}
     const handleLogout = () => {
         navigate('/login'); 
     }
@@ -13,8 +13,8 @@ const Dashboard = () => {
         <div className="dashboard-container">
             {/* Dashboard component's content goes here */}
             <h2>Coming soon :)</h2>
-            {/* Button to log out */}
-            <button className="logout-button" onClick={handleLogout}>Logout</button> {/* Apply the class here */}
+            {/* Clickable text for logging out */}
+            <span className="logout-text" onClick={handleLogout}>Logout</span> {/* Using span with className for the clickable text */}
         </div>
     );
 }
