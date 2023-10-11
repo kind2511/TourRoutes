85cda50a63a92275a32bed2b8bbbd421b2fe95b0
ahmadmm <ahmadmm@stud.ntnu.no>
2023-10-11 21:03:08 +0200

    Added Mapbox integration to the dashboard for enhanced geographical visualization

--- turorienteringapp/frontend/src/components/Dashboard.js
+++ turorienteringapp/frontend/src/components/Dashboard.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Dashboard.css';
+// Mapbox GL JS library import
+import mapboxgl from 'mapbox-gl';
 
+// Our access tokens
+mapboxgl.accessToken = 'pk.eyJ1IjoiY2hyaXNhMjUxMSIsImEiOiJjbGtkcjRhNnkwa3JhM2t1ODFtbHppd2JmIn0.9DC6eUXzdFclnzb_3LCOtg';  
+
+/*function for component Dashboard*/
 const Dashboard = () => {
-    /* Initializing the navigate function for navigation purposes */
+
+    /*create ref to dom element, to where should render the map in the container in the dashboard*/
+    const mapContainerRef = useRef(null);  
+
+    /* create navigation function to navigate between routes in dashboard page*/
     const navigate = useNavigate();
 
     /* Navigate to the UserProfile page */
@@ -21,6 +31,20 @@ const Dashboard = () => {
         navigate('/about');
     }
 
+    // Mapbox map initialization
+    useEffect(() => {
+        const map = new mapboxgl.Map({
+            container: mapContainerRef.current,// ref to container in dashboard where should be show
+            style: 'mapbox://styles/mapbox/streets-v11',// style map
+            center: [10.797379, 60.794533],  // Initial position set to Gjøvik, Norway.
+            zoom: 9 //zoom level
+        });
+
+        // Cleanup on component unmount--> clean it after use
+        return () => map.remove();
+    }, []);
+
+    // JSX return for the Dashboard component
     return (
         <div className="dashboard-container">
             {/* Navigation bar options for the dashboard */}
@@ -32,7 +56,7 @@ const Dashboard = () => {
                 <span className="option-item" onClick={handleLogout}>Logout</span>
             </div>
             {/* Placeholder for the main content of the dashboard (e.g., map view) */}
-            <div className="map-placeholder"></div>
+            <div className="map-placeholder" ref={mapContainerRef}></div>  {/* Adding reference for Mapbox */}
         </div>
     );
 }
