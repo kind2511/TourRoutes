d964faec14bac2f9e31cf7b7246c474bdaf6c5b6
ahmadmm <ahmadmm@stud.ntnu.no>
2023-10-12 15:29:18 +0200

    feat(user-profile): Add user profile functionality and UI components

--- turorienteringapp/frontend/src/components/UserProfile.js
+++ turorienteringapp/frontend/src/components/UserProfile.js
@@ -1,42 +1,88 @@
-/* test test test*/
-
 import React, { useState, useEffect } from 'react';
 import './UserProfile.css';
 
 const UserProfile = () => {
+    // State to store user's profile data
     const [userData, setUserData] = useState({});
 
+    // State to manage the edit mode for each individual field
+    const [isEditing, setIsEditing] = useState({ 
+        firstName: false, 
+        lastName: false, 
+        email: false 
+    });
+
+    /*backend-----> :)*/
     useEffect(() => {
-        const fetchUserData = async () => {
-            try {
-                const token = localStorage.getItem('token');
-                const response = await fetch('http://localhost:8000/api/v1/users/profile', {
-                    headers: { 'Authorization': `Bearer ${token}` }
-                });
-
-                const data = await response.json();
-                if (data.status === 'success') {
-                    setUserData(data.user);
-                } else {
-                    console.error(data.message);
-                }
-            } catch (error) {
-                console.error('Error fetching user data:', error);
-            }
-        }
-
-        fetchUserData();
+        /* 
+           TODO: Fetch the user's profile data from the backend when the component mounts.
+           Use an API call to get the data and then update the userData state.
+        */
     }, []);
 
-    return (
-        <div className="user-profile-container">
-            <h2>User Profile</h2>
-            <p><strong>First Name:</strong> {userData.firstName}</p>
-            <p><strong>Last Name:</strong> {userData.lastName}</p>
-            <p><strong>Email:</strong> {userData.email}</p>
-            {/* Add any other user properties as needed */}
+    /**
+     * Update a user's profile data and reset the edit mode for the field.
+     */
+    const handleUpdateProfile = (field) => {
+        setIsEditing(prevEditingState => ({ 
+            ...prevEditingState, 
+            [field]: false 
+        }));
+        
+        /* 
+           TODO: Send the updated data to the backend.
+           Use an API call to send the new data (from userData) to update the user profile.
+        */
+    };
+
+    /*
+     * Render an editable field with options to save or switch to edit mode.
+     *
+     * @param {string} field - The name of the field (e.g., "firstName")
+     * @param {string} value - The current value of the field
+     * @return {JSX.Element} - A React element that represents the editable field
+     */
+    const renderEditableField = (field, value) => (
+        <div>
+            {isEditing[field] ? (
+                <>
+                    <input
+                        value={value}
+                        onChange={(e) => setUserData(prevData => ({ 
+                            ...prevData, 
+                            [field]: e.target.value 
+                        }))}
+                    />
+                    <button onClick={() => handleUpdateProfile(field)}>Save</button>
+                </>
+            ) : (
+                <>
+                    <span>{value}</span>
+                    <div className="edit-dropdown">
+                        <span>Edit</span>
+                        <div className="edit-dropdown-content">
+                            <a onClick={() => setIsEditing(prevEditingState => ({ 
+                                ...prevEditingState, 
+                                [field]: true 
+                            }))}>Edit</a>
+                        </div>
+                    </div>
+                </>
+            )}
         </div>
     );
-}
+
+    {/*Render*/}
+    return (
+        <div className="user-profile-background">
+            <div className="user-profile-container">
+                <h2>User Profile</h2>
+                <p><strong>First Name:</strong> {renderEditableField("firstName", userData.firstName)}</p>
+                <p><strong>Last Name:</strong> {renderEditableField("lastName", userData.lastName)}</p>
+                <p><strong>Email:</strong> {renderEditableField("email", userData.email)}</p>
+            </div>
+        </div>
+    );
+};
 
 export default UserProfile;
