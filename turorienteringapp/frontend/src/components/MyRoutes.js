ee68813cb765eb668b983eb2c9694ae2f3bd3ac3
ahmadmm <ahmadmm@stud.ntnu.no>
2023-10-25 15:47:07 +0200

    Add distance calculation for route coordinates

--- turorienteringapp/frontend/src/components/MyRoutes.js
+++ turorienteringapp/frontend/src/components/MyRoutes.js
@@ -1,17 +1,33 @@
 import React, { useEffect, useState, useRef } from 'react';
 import mapboxgl from 'mapbox-gl';
 import axios from 'axios'; // Library for making HTTP requests
+import { point, distance } from '@turf/turf';
 import './MyRoutes.css';
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiY2hyaXNhMjUxMSIsImEiOiJjbGtkcjRhNnkwa3JhM2t1ODFtbHppd2JmIn0.9DC6eUXzdFclnzb_3LCOtg';
 
-//Color for routes
+// Color for routes
 const colors = ['#F00', '#0F0', '#00F', '#FF0', '#0FF', '#F0F', '#888', '#123456', '#654321', '#ABCDEF'];
 
 const MyRoutes = () => {
     const mapContainerRef = useRef(null); // where to render the map
     const [routes, setRoutes] = useState([]); // manage the fetched routes
  
+    // ----------------------------------------->Function to calculate the distance of a route
+    const calculateRouteDistance = (coordinates) => {
+        let totalDistance = 0;  // Initialize total distance
+    
+        for (let i = 0; i < coordinates.length - 1; i++) {
+            const from = point(coordinates[i]);  // Convert current coordinate to point
+            const to = point(coordinates[i + 1]);  // Convert next coordinate to point
+            const options = { units: 'kilometers' };  // Define distance measurement unit
+            totalDistance += distance(from, to, options);  // Calculate and accumulate distance
+        }
+    
+        return totalDistance.toFixed(2);  // Return total distance with 2 decimal places
+    };
+    
+ //----------------------------------------------->
     /*
      * For fetching data and setting up the map
      */
@@ -49,7 +65,7 @@ const MyRoutes = () => {
             zoom: 9
         });
         
-        //displays the user's routes on the map
+        // Displays the user's routes on the map
         map.on('load', () => {
             routes.forEach((route, index) => {
                 if (route.coordinates && route.coordinates.length > 0) {
@@ -67,7 +83,7 @@ const MyRoutes = () => {
                             }
                         });
                          
-                        //Adds a line layer to the map for the current route.
+                        // Adds a line layer to the map for the current route.
                         map.addLayer({
                             id: `layer-${route.name}`,
                             type: 'line',
@@ -89,7 +105,7 @@ const MyRoutes = () => {
                             .setLngLat(coord)
                             .addTo(map);
                             
-                        // Add the route name for the first and last point of each route
+                        // Add the route name and distance for the first and last point of each route
                         if(coordIndex === 0 || coordIndex === route.coordinates.length - 1) {
                             new mapboxgl.Popup({
                                 closeOnClick: false,
@@ -97,7 +113,7 @@ const MyRoutes = () => {
                                 offset: 25
                             })
                             .setLngLat(coord)
-                            .setHTML(route.name)
+                            .setHTML(`${route.name} - ${calculateRouteDistance(route.coordinates)} km`)
                             .addTo(map);
                         }
                     });
@@ -110,7 +126,7 @@ const MyRoutes = () => {
 
     }, [routes]);
 
-     // Render the map container
+    // Render the map container
     return (
         <div className="mapContainer" ref={mapContainerRef}></div>
     );
