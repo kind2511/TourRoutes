d5c44fb2eb645fb9596b2b1ca0a8724db936ff22
ahmadmm <ahmadmm@stud.ntnu.no>
2023-10-27 00:23:31 +0200

    Added MyRoutes logo and positioning

--- turorienteringapp/frontend/src/components/MyRoutes.js
+++ turorienteringapp/frontend/src/components/MyRoutes.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState, useRef } from 'react';
 import mapboxgl from 'mapbox-gl';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 import './MyRoutes.css';
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiY2hyaXNhMjUxMSIsImEiOiJjbGtkcjRhNnkwa3JhM2t1ODFtbHppd2JmIn0.9DC6eUXzdFclnzb_3LCOtg';
@@ -9,6 +10,7 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiY2hyaXNhMjUxMSIsImEiOiJjbGtkcjRhNnkwa3JhM2t1O
 const colors = ['#F00', '#0F0', '#00F', '#FF0', '#0FF', '#F0F', '#888', '#123456', '#654321', '#ABCDEF'];
 
 const MyRoutes = () => {
+    const navigate = useNavigate();
     const mapContainerRef = useRef(null);
     const [routes, setRoutes] = useState([]);
     const [error, setError] = useState(null); // For displaying errors
@@ -22,7 +24,7 @@ const MyRoutes = () => {
             setError("There was a problem fetching the routes. Please try again.");
         }
     };
-    
+
     useEffect(() => {
         fetchRoutes();
     }, []);
@@ -73,16 +75,16 @@ const MyRoutes = () => {
                 // Create styled popup for start and end of route
                 const startCoord = route.coordinates[0];
                 const endCoord = route.coordinates[route.coordinates.length - 1];
-                
+
                 [startCoord, endCoord].forEach((coord) => {
                     new mapboxgl.Popup({
                         closeOnClick: false,
                         closeButton: false,
                         className: 'route-name-popup' // Custom class for styling
                     })
-                    .setLngLat(coord)
-                    .setHTML(`<div class="route-name-container">${route.name}</div>`)
-                    .addTo(map);
+                        .setLngLat(coord)
+                        .setHTML(`<div class="route-name-container">${route.name}</div>`)
+                        .addTo(map);
                 });
             });
         });
@@ -91,10 +93,18 @@ const MyRoutes = () => {
 
     }, [routes]);
 
+    const handleLogoClick = () => {
+        navigate('/dashboard');
+    }
+
     return (
         <div className="relativeContainer">
-            {error && <div className="errorNotification">{error}</div>} {/* Displaying errors */}
-            <div className="mapContainer" ref={mapContainerRef}></div>
+            {error && <div className="errorNotification">{error}</div>}
+
+            <div className="mapContainer" ref={mapContainerRef}>
+                {/* TurRuter logo positioned above the map */}
+                <div className="myroutes-logo" onClick={handleLogoClick}>TurRuter</div>
+            </div>
         </div>
     );
 }
