239ddef40cf995b1470cdd7c912a38c1962d0b5c
ahmadmm <ahmadmm@stud.ntnu.no>
2023-10-31 14:28:02 +0100

    Enhance FindPath: Add routing functionality using Mapbox and refine location suggestions

--- turorienteringapp/frontend/src/components/FindPath.js
+++ turorienteringapp/frontend/src/components/FindPath.js
@@ -3,12 +3,16 @@ import { useNavigate } from 'react-router-dom';
 import mapboxgl from 'mapbox-gl';
 import './FindPath.css';
 
+// Set the Mapbox access token
 mapboxgl.accessToken = 'pk.eyJ1IjoiY2hyaXNhMjUxMSIsImEiOiJjbGtkcjRhNnkwa3JhM2t1ODFtbHppd2JmIn0.9DC6eUXzdFclnzb_3LCOtg';
 
 const FindPath = () => {
+    // Refrences to map container_map instance_ navigation hook
     const mapContainerRef = useRef(null);
     const mapRef = useRef(null);
     const navigate = useNavigate();
+
+    //State varirables to input start/end point & location suggestions start/end
     const [startPoint, setStartPoint] = useState('');
     const [endPoint, setEndPoint] = useState('');
     const [startSuggestions, setStartSuggestions] = useState([]);
@@ -39,16 +43,86 @@ const FindPath = () => {
         setter(data.features.map(feat => feat.place_name));
     };
 
+    //fetch the coordinates from the given address name
+    const getCoordinatesFromName = async (name) => {
+        const apiUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(name)}.json?access_token=${mapboxgl.accessToken}`;
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        return data.features[0].center;
+    };
+    // fetch drive route between two put of cordinates
+    const getRoute = async (start, end) => {
+        try {
+            const apiUrl = `https://api.mapbox.com/directions/v5/mapbox/driving/${start.join(',')};${end.join(',')}?geometries=geojson&access_token=${mapboxgl.accessToken}`;
+            const response = await fetch(apiUrl);
+            const data = await response.json();
+            if (data.code !== "Ok") {
+                throw new Error(`Failed to fetch route: ${data.code}`);
+            }
+            return data.routes[0].geometry.coordinates;
+        } catch (error) {
+            console.error('Error in getRoute:', error);
+            throw error;
+        }
+    };
+
+    /*
+     * Function find path button, fetch the routes and update map
+     */
+    const handleFindPath = async () => {
+        try {
+            const startCoords = await getCoordinatesFromName(startPoint);
+            const endCoords = await getCoordinatesFromName(endPoint);
+            const route = await getRoute(startCoords, endCoords);
+
+            if (mapRef.current.getSource('route')) {
+                mapRef.current.removeLayer('route');
+                mapRef.current.removeSource('route');
+            }
+
+            mapRef.current.addSource('route', {
+                type: 'geojson',
+                data: {
+                    type: 'Feature',
+                    properties: {},
+                    geometry: {
+                        type: 'LineString',
+                        coordinates: route
+                    }
+                }
+            });
+
+            mapRef.current.addLayer({
+                id: 'route',
+                type: 'line',
+                source: 'route',
+                layout: {},
+                paint: {
+                    'line-width': 5,
+                    'line-color': '#ff0000'
+                }
+            });
+
+            // Adjust map to fit the route
+            const bounds = route.reduce((bounds, coord) => {
+                return bounds.extend(coord);
+            }, new mapboxgl.LngLatBounds(route[0], route[0]));
+
+            mapRef.current.fitBounds(bounds, {
+                padding: 20
+            });
+
+        } catch (error) {
+            console.error('Error in handleFindPath:', error);
+        }
+    };
+
     // Navigate back to the dashboard
     const handleLogoClick = () => {
         navigate('/dashboard');
-    }
+    };
 
-    const handleFindPath = () => {
-        /*------> TODO: this logic will be added later -------> */
-    }
-
-
+    //JSX for render the component
     return (
         <div className="fullMapContainer">
             <div ref={mapContainerRef} className="fullMap">
