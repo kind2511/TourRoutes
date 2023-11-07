1cf8631f9e4568f8f4c7382ead8adc1143c1edbc
kind2511 <chrisa2511@gmail.com>
2023-11-07 13:09:16 +0100

    worked on tetsing

--- turorienteringapp/backend/__tests__/user.test.js
+++ turorienteringapp/backend/__tests__/user.test.js
@@ -1,4 +1,17 @@
 const userHandler = require("../handlers/userHandler");
 
-it("should send a status code of 400 when user exists", () => {});
+const request = {
+  body: {
+    firstName: "mockFirstName",
+    lastName: "monckLastName",
+    email: "mockEmail",
+    password: "mockPassword",
+    confirmPassword: "mockConfirmPassword",
+    role: "user",
+  },
+};
+
+it("test", async () => {
+  await userHandler.signup(request);
+});
 
