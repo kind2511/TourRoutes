14ea38832617c31d00121591bf503f66afd68572
ahmadmm <ahmadmm@stud.ntnu.no>
2023-11-08 21:17:56 +0100

    Import the Map and fit it to whole My Routes page

--- turorienteringapp/frontend/src/components/MyRoutes.js
+++ turorienteringapp/frontend/src/components/MyRoutes.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import mapboxgl from 'mapbox-gl';
 import './MyRoutes.css';
 
+// Set the Mapbox access token
+mapboxgl.accessToken = 'pk.eyJ1IjoiY2hyaXNhMjUxMSIsImEiOiJjbGtkcjRhNnkwa3JhM2t1ODFtbHppd2JmIn0.9DC6eUXzdFclnzb_3LCOtg';
+
+
 const MyRoutes = () => {
-  return (
-    <div className="my-routes">
-      <h1>My Routes</h1>
-      {/* Content for My Routes will applied here :)*/}
-    </div>
-  );
+    const mapContainerRef = useRef(null);
+
+    // Initialize the map and plot routes
+    useEffect(() => {
+        const map = new mapboxgl.Map({
+            container: mapContainerRef.current,
+            style: 'mapbox://styles/mapbox/streets-v11',
+            center: [10.797379, 60.794533],
+            zoom: 9
+        });
+
+        // Clean up on unmount
+        return () => map.remove();
+    }, []);
+
+    return (
+        <div className="mapContainer" ref={mapContainerRef} />
+    );
 };
 
 export default MyRoutes;
-
