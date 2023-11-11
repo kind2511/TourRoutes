f014b78eabc7269b0ffe2c13f9af1ab80ee655e5
kind2511 <chrisa2511@gmail.com>
2023-11-11 15:55:32 +0100

    unit testing of users

---
+++ turorienteringapp/backend/__tests__/auth.test.js
@@ -0,0 +1,1 @@
+// Unit testing auth handlers
--- turorienteringapp/backend/__tests__/user.test.js
+++ turorienteringapp/backend/__tests__/user.test.js
@@ -1,17 +1,42 @@
-const userHandler = require("../handlers/userHandler");
+const { getUsers } = require("../handlers/userHandler");
+const User = require("../models/usersModel");
 
-const request = {
-  body: {
-    firstName: "mockFirstName",
-    lastName: "monckLastName",
-    email: "mockEmail",
-    password: "mockPassword",
-    confirmPassword: "mockConfirmPassword",
-    role: "user",
-  },
-};
+// Mocks the User Model and the request object
+jest.mock("../models/usersModel");
+const mockJson = jest.fn();
+const mockStatus = jest.fn().mockImplementation(() => ({ json: mockJson }));
+const res = { status: mockStatus };
 
-it("test", async () => {
-  await userHandler.signup(request);
+describe("getUsers handler", () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test("should handle success case", async () => {
+    const mockUsers = [{ name: "User1" }, { name: "User2" }];
+    User.find.mockResolvedValue(mockUsers);
+
+    await getUsers(null, res);
+
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(mockJson).toHaveBeenCalledWith({
+      status: "success",
+      data: {
+        users: mockUsers,
+      },
+    });
+  });
+
+  test("should handle error case", async () => {
+    const errorMessage = "Error fetching users";
+    User.find.mockRejectedValue(errorMessage);
+
+    await getUsers(null, res);
+
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(mockJson).toHaveBeenCalledWith({
+      status: "fail",
+      message: errorMessage,
+    });
+  });
 });
-
