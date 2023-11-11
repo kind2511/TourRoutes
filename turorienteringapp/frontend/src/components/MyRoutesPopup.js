db24180163b5abef9adde6df1f690c4e082b3518
ahmadmm <ahmadmm@stud.ntnu.no>
2023-11-11 12:31:04 +0100

    Add dropdown to MyRoutesPopup and adjust styling

--- turorienteringapp/frontend/src/components/MyRoutesPopup.js
+++ turorienteringapp/frontend/src/components/MyRoutesPopup.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './MyRoutesPopup.css';
 
 const MyRoutesPopup = ({ route, distance }) => {
+
+    //useState to manage the select options
+    const [selectedOption, setSelectedOption] = useState('');
+
+    // Handler for change options on dropdown
+    const handleChange = (event) => {
+        setSelectedOption(event.target.value);
+    };
+
     return (
         <div className="my-routes-popup-container">
+            <select className="dropdown" value={selectedOption} onChange={handleChange}>
+                <option value="" hidden></option>
+                <option value="Option1">:)</option>
+                <option value="Option2">:)</option>
+            </select>
             <div className="my-routes-name">{route.name}</div>
             <div className="my-routes-distance">{distance} km</div>
         </div>
