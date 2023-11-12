3b8f169e3968a5666bebd7157721a785f1bcaa10
ahmadmm <ahmadmm@stud.ntnu.no>
2023-11-12 21:16:12 +0100

    Add privacy toggle and update MyRoutesPopup for simpler layout

--- turorienteringapp/frontend/src/components/MyRoutesPopup.js
+++ turorienteringapp/frontend/src/components/MyRoutesPopup.js
@@ -1,17 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react'; 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; // Importing icon component from FontAwesome
-import { faTrash } from '@fortawesome/free-solid-svg-icons'; // Importing trash icon from FontAwesome solid icon set
+import { faTrash } from '@fortawesome/free-solid-svg-icons'; // Importing trash icon from FontAwesome 
 import './MyRoutesPopup.css';
 
+
 const MyRoutesPopup = ({ route, distance, onDelete }) => {
+
+    // State for route's privacy (private/public)
+    const [isPrivate, setIsPrivate] = useState(route.isPrivate);
+
+    // Deletes the route
     const handleDeleteClick = () => {
         onDelete(route.id);
+    };
+
+    // Handles route visibility change
+    const handleVisibilityChange = (event) => {
+        setIsPrivate(event.target.value === 'private');
+        // TOdo: the visibiluty will be update for backend 
     };
 
     return (
         <div className="my-routes-popup-container">
             <div className="my-routes-name">{route.name}</div>
             <div className="my-routes-distance">{distance} km</div>
+            <div className="privacy-toggle">
+                <label>
+                    <input
+                        type="radio"
+                        name="visibility"
+                        value="public"
+                        checked={!isPrivate}
+                        onChange={handleVisibilityChange}
+                    />
+                    Public
+                </label>
+                <label>
+                    <input
+                        type="radio"
+                        name="visibility"
+                        value="private"
+                        checked={isPrivate}
+                        onChange={handleVisibilityChange}
+                    />
+                    Private
+                </label>
+            </div>
             <span className="my-routes-delete-icon" onClick={handleDeleteClick}>
                 <FontAwesomeIcon icon={faTrash} />
             </span>
