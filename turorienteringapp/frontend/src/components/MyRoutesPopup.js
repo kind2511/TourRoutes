c85ca36413b5d925afe83b53788bcaeb3c5d9a45
ahmadmm <ahmadmm@stud.ntnu.no>
2023-11-12 15:48:04 +0100

    Add delete option to MyRoutesPopup dropdown and implement deletion logic

--- turorienteringapp/frontend/src/components/MyRoutesPopup.js
+++ turorienteringapp/frontend/src/components/MyRoutesPopup.js
@@ -1,14 +1,25 @@
 import React, { useState } from 'react';
 import './MyRoutesPopup.css';
 
-const MyRoutesPopup = ({ route, distance }) => {
+const MyRoutesPopup = ({ route, distance, onDelete }) => {
 
     //useState to manage the select options
     const [selectedOption, setSelectedOption] = useState('');
 
+
     // Handler for change options on dropdown
     const handleChange = (event) => {
         setSelectedOption(event.target.value);
+
+        // If the selected option is 'Delete', call the deletion handler
+        if (event.target.value === 'Delete') {
+            handleDelete();
+        }
+    };
+
+    const handleDelete = () => {
+        // Call the onDelete 
+        onDelete(route.id);
     };
 
     return (
@@ -16,7 +27,7 @@ const MyRoutesPopup = ({ route, distance }) => {
             <select className="dropdown" value={selectedOption} onChange={handleChange}>
                 <option value="" hidden></option>
                 <option value="Option1">:)</option>
-                <option value="Option2">:)</option>
+                <option value="Delete">Delete</option>
             </select>
             <div className="my-routes-name">{route.name}</div>
             <div className="my-routes-distance">{distance} km</div>
