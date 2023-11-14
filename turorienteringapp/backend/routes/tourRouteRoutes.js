88cd642a6edd3720a751cfa6a5cf43d9c21836f9
kind2511 <chrisa2511@gmail.com>
2023-11-14 13:30:27 +0100

    changed to control model view

--- turorienteringapp/backend/routes/tourRouteRoutes.js
+++ turorienteringapp/backend/routes/tourRouteRoutes.js
@@ -1,35 +1,44 @@
 const express = require("express");
-const tourRouteHandler = require("../handlers/tourRouteHandler");
-const userHandler = require("../handlers/userHandler");
-
+const authentificationController = require("../controllers/authentificationContoller");
+const userController = require("../controllers/userController");
+const tourRouteController = require("../controllers/tourRouteController");
 const router = express.Router();
 
 // Route to create a new tour route
 router
   .route("/newTourRoute")
-  .post(userHandler.authenticate, tourRouteHandler.newTourRoute);
+  .post(
+    authentificationController.authenticate,
+    tourRouteController.newTourRoute
+  );
 
 // Route to get all individual routes of one user
 router
   .route("/getIndividualUsersTourRoutes")
-  .get(userHandler.authenticate, tourRouteHandler.getIndividualUsersTourRoutes);
+  .get(
+    authentificationController.authenticate,
+    tourRouteController.getIndividualUsersTourRoutes
+  );
 
 // Route to get all published tour routes
 router
   .route("/getAllPublishedTourRoutes")
-  .get(userHandler.authenticate, tourRouteHandler.getAllPublishedTourRoutes);
+  .get(
+    authentificationController.authenticate,
+    tourRouteController.getAllPublishedTourRoutes
+  );
 
 //-------------------------------------------------------------------------------------------------------------
 // Routes for Admin
 
 // Routes to get all tour routes
-router.route("/").get(tourRouteHandler.getAllTourRoutes);
+router.route("/").get(tourRouteController.getAllTourRoutes);
 
 // Route to handle operations based on tour route id
 router
   .route("/:id")
-  .get(tourRouteHandler.getTourRoute)
-  .delete(tourRouteHandler.deleteTourRoute);
+  .get(tourRouteController.getTourRoute)
+  .delete(tourRouteController.deleteTourRoute);
 
 // Exports markerRoutes into the main application
 module.exports = router;
--- turorienteringapp/backend/routes/userRoutes.js
+++ turorienteringapp/backend/routes/userRoutes.js
@@ -1,59 +1,82 @@
 const express = require("express");
-const userHandler = require("../handlers/userHandler");
+const userController = require("../controllers/userController");
+const authentificationController = require("../controllers/authentificationContoller");
+
 const router = express.Router();
 
 // Route to handle user registration
-router.route("/register").post(userHandler.signup);
+router.route("/register").post(authentificationController.signup);
 
 // Route to handle user login
-router.route("/login").post(userHandler.login);
+router.route("/login").post(authentificationController.login);
 
 // Route to get info about current loged in user
 router
   .route("/myProfile")
-  .get(userHandler.authenticate, userHandler.myProfile, userHandler.getUser);
+  .get(
+    authentificationController.authenticate,
+    userController.myProfile,
+    userController.getUser
+  );
 
 // Route to update name and email of current user
 router
   .route("/updateMyProfile")
-  .patch(userHandler.authenticate, userHandler.updateMyProfile);
+  .patch(
+    authentificationController.authenticate,
+    userController.updateMyProfile
+  );
 
 // Route to update the password of the current user
 router
   .route("/updateMyPassword")
-  .patch(userHandler.authenticate, userHandler.updateMyPassword);
+  .patch(
+    authentificationController.authenticate,
+    userController.updateMyPassword
+  );
 
 // Route to delete the current user
 router
   .route("/deleteMyProfile")
-  .delete(userHandler.authenticate, userHandler.deleteMyProfile);
+  .delete(
+    authentificationController.authenticate,
+    userController.deleteMyProfile
+  );
 
 // ----------------------------------------------------------------------------------------------------------------------
 // Routes for Admin
 // ----------------------------------------------------------------------------------------------------------------------
 
-// Route to promote user to admin 
+// Route to promote user to admin
 router
   .route("/promoteToAdmin/:id")
   .patch(
-    userHandler.authenticate,
-    userHandler.isAdmin,
-    userHandler.promoteToAdmin
+    authentificationController.authenticate,
+    authentificationController.isAdmin,
+    userController.promoteToAdmin
   );
 
-// Route to get all users 
+// Route to get all users
 router
   .route("/")
-  .get(userHandler.authenticate, userHandler.isAdmin, userHandler.getUsers);
+  .get(
+    authentificationController.authenticate,
+    authentificationController.isAdmin,
+    userController.getUsers
+  );
 
-// Routes to handle operations based on user id 
+// Routes to handle operations based on user id
 router
   .route("/:id")
-  .get(userHandler.authenticate, userHandler.isAdmin, userHandler.getUser)
+  .get(
+    authentificationController.authenticate,
+    authentificationController.isAdmin,
+    userController.getUser
+  )
   .delete(
-    userHandler.authenticate,
-    userHandler.isAdmin,
-    userHandler.deleteUser
+    authentificationController.authenticate,
+    authentificationController.isAdmin,
+    userController.deleteUser
   );
 
 module.exports = router;
