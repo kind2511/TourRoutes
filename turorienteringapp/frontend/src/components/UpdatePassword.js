10a8551a72151a6338785681dbfda94295731a56
ahmadmm <ahmadmm@stud.ntnu.no>
2023-11-15 12:06:19 +0100

    Implement password update functionality in UpdatePassword component

--- turorienteringapp/frontend/src/components/UpdatePassword.js
+++ turorienteringapp/frontend/src/components/UpdatePassword.js
@@ -1,37 +1,57 @@
 import React, { useState } from "react";
-import "./UpdatePassword.css"; 
+import "./UpdatePassword.css";
 
 const UpdatePassword = ({ close }) => {
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
 
-  const handleUpdate = async () => {
-  //TODO logic to update password here!
-    console.log("Password Updated:", password);
-    close();
-  };
+    const handleUpdate = async () => {
+        // Proceed with the API call
+        try {
+            const response = await fetch('http://localhost:8000/api/v1/users/updateMyPassword', {
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${localStorage.getItem('token')}`,
+                },
+                body: JSON.stringify({ password, confirmPassword }),
+            });
 
-  return (
-    <div className="update-password-container">
-      <h1>Update Password</h1>
-      <input
-        type="password"
-        placeholder="New Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Confirm New Password"
-        value={confirmPassword}
-        onChange={(e) => setConfirmPassword(e.target.value)}
-      />
-      <div className="update-password-buttons">
-        <button className="update-password-btn" onClick={handleUpdate}>Update Password</button>
-        <button className="cancel-btn" onClick={close}>Cancel</button>
-      </div>
-    </div>
-  );
+            const data = await response.json();
+            if (response.ok) {
+                // Password updated successfully
+                close(); // Close the update password dialog
+            } else {
+
+                console.error("Failed to update password:", data.message);
+            }
+        } catch (err) {
+
+            console.error("Error updating password:", err);
+        }
+    };
+
+    return (
+        <div className="update-password-container">
+            <h1>Update Password</h1>
+            <input
+                type="password"
+                placeholder="New Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+            <input
+                type="password"
+                placeholder="Confirm New Password"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+            <div className="update-password-buttons">
+                <button className="update-password-btn" onClick={handleUpdate}>Update Password</button>
+                <button className="cancel-btn" onClick={close}>Cancel</button>
+            </div>
+        </div>
+    );
 };
 
 export default UpdatePassword;
