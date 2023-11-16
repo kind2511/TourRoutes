e83bb0f70250878cfdf22d13722bebafccb6a8fe
ahmadmm <ahmadmm@stud.ntnu.no>
2023-11-16 16:59:03 +0100

    Refactor Mapbox integration, update MyRoutes styles, remove MyRoutesPopup component, add delete route feature

--- turorienteringapp/frontend/src/components/MyRoutes.js
+++ turorienteringapp/frontend/src/components/MyRoutes.js
@@ -2,15 +2,13 @@ import React, { useEffect, useState, useRef } from 'react';
 import mapboxgl from 'mapbox-gl';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import ReactDOMServer from 'react-dom/server';
 import { length } from '@turf/turf'; // for distance calculation
 import './MyRoutes.css';
-import MyRoutesPopup from './MyRoutesPopup'; // MyRoutes info
 
 // Set the Mapbox access token
 mapboxgl.accessToken = 'pk.eyJ1IjoiY2hyaXNhMjUxMSIsImEiOiJjbGtkcjRhNnkwa3JhM2t1ODFtbHppd2JmIn0.9DC6eUXzdFclnzb_3LCOtg';
 
-//colors
+// Colors for route lines
 const colors = ['#F00', '#0F0', '#00F', '#FF0', '#0FF', '#F0F', '#888', '#123456', '#654321', '#ABCDEF'];
 
 const MyRoutes = () => {
@@ -21,9 +19,25 @@ const MyRoutes = () => {
 
     //-------------------------------------------------------------->
 
-    /* 
-     *Function to fetch the authenticated user routes 
-     */
+    // Function to delete a route
+    const deleteRoute = async (routeId) => {
+        const userToken = localStorage.getItem('token');
+        if (!userToken) {
+            setError("Login required to perform this action.");
+            return;
+        }
+
+        try {
+            await axios.delete(`http://localhost:8000/api/v1/tourRoutes/${routeId}`, {
+                headers: { 'Authorization': `Bearer ${userToken}` }
+            });
+            // Remove the route from the local state to update the UI
+            setRoutes(routes.filter(route => route.id !== routeId));
+        } catch (err) {
+            setError("Error deleting route.");
+            console.error(err);
+        }
+    };
 
     // Fetch user's routes if logged in
     const fetchRoutes = async () => {
@@ -38,97 +52,136 @@ const MyRoutes = () => {
                 headers: { 'Authorization': `Bearer ${userToken}` }
             });
             setRoutes(response.data.data.tourRoutes || []);
-        } catch (err) { // Handle errors if the routes fetch fails
+        } catch (err) {
+            // Handle errors if the routes fetch fails
             setError("Error fetching routes.");
             console.error(err);
         }
     };
 
-    // Execute fetchRoutes once  component mount
+    // Execute fetchRoutes once component mounts
     useEffect(() => {
         fetchRoutes();
     }, []);
-
 
     //-------------------------------------------------------------->
 
     // Initialize the map and plot routes
     useEffect(() => {
-        const map = new mapboxgl.Map({
-            container: mapContainerRef.current,
-            style: 'mapbox://styles/mapbox/streets-v11',
-            center: [10.797379, 60.794533],
-            zoom: 9
-        });
-
-        // Plot each route on the map once it's loaded
-        map.on('load', () => {
-            routes.forEach((route, index) => {
-                if (!route.coordinates || route.coordinates.length === 0) {
-                    console.warn("Route without valid coordinates:", route);
-                    return;
-                }
-
-                const sourceName = `source-${route.name}`;
-                if (!map.getSource(sourceName)) {
-                    map.addSource(sourceName, {
-                        type: 'geojson',
-                        data: {
-                            type: 'Feature',
-                            geometry: {
-                                type: 'LineString',
-                                coordinates: route.coordinates
-                            }
-                        }
-                    });
-
-                    map.addLayer({
-                        id: `layer-${route.name}`,
-                        type: 'line',
-                        source: sourceName,
-                        layout: {
-                            'line-join': 'round',
-                            'line-cap': 'round'
-                        },
-                        paint: {
-                            'line-color': colors[index % colors.length],
-                            'line-width': 8
-                        }
-                    });
-                }
-
-                // Calculate distance for the route
-                const distance = length({ type: 'Feature', geometry: { type: 'LineString', coordinates: route.coordinates } }, { units: 'kilometers' });
-                const MyPopupContent = ReactDOMServer.renderToString(<MyRoutesPopup route={route} distance={distance.toFixed(2)} />);
-
-                const startCoord = route.coordinates[0];
-
-                // Create a popup only for the start coordinate of the route
-                new mapboxgl.Popup({
-                    closeOnClick: false,
-                    closeButton: false,
-                    className: 'route-name-popup'
-                })
-                    .setLngLat(startCoord)
-                    .setHTML(MyPopupContent)
-                    .addTo(map);
+        const initializeMap = () => {
+            const map = new mapboxgl.Map({
+                container: mapContainerRef.current,
+                style: 'mapbox://styles/mapbox/streets-v11',
+                center: [10.797379, 60.794533],
+                zoom: 9
             });
-        });
 
-        // Clean up map instance on component unmount
-        return () => map.remove();
+            // Plot each route on the map once it's loaded
+            map.on('load', () => {
+                routes.forEach((route, index) => {
+                    if (!route.coordinates || route.coordinates.length === 0) {
+                        console.warn("Route without valid coordinates:", route);
+                        return;
+                    }
+
+                    const sourceName = `source-${route.name}`;
+                    if (!map.getSource(sourceName)) {
+                        map.addSource(sourceName, {
+                            type: 'geojson',
+                            data: {
+                                type: 'Feature',
+                                geometry: {
+                                    type: 'LineString',
+                                    coordinates: route.coordinates
+                                }
+                            }
+                        });
+
+                        map.addLayer({
+                            id: `layer-${route.name}`,
+                            type: 'line',
+                            source: sourceName,
+                            layout: {
+                                'line-join': 'round',
+                                'line-cap': 'round'
+                            },
+                            paint: {
+                                'line-color': colors[index % colors.length],
+                                'line-width': 8
+                            }
+                        });
+                    }
+                    // Calculate distance for the route
+                    const distance = length(
+                        {
+                            type: 'Feature', geometry:
+                                { type: 'LineString', coordinates: route.coordinates }
+                        }, { units: 'kilometers' });
+
+                    //----------------------------------------------->
+
+                    /*
+                     * HTML elements that will render a popup containing information about a route on the map
+                     */
+
+                    // Create popup content as container
+                    const popupDiv = document.createElement('div');
+                    popupDiv.className = 'my-routes-popup';
+
+                    // Route name
+                    const routeNameDiv = document.createElement('div');
+                    routeNameDiv.className = 'my-routes-name';
+                    routeNameDiv.textContent = route.name;
+                    popupDiv.appendChild(routeNameDiv);
+
+                    // Route distance
+                    const routeDistanceDiv = document.createElement('div');
+                    routeDistanceDiv.className = 'my-routes-distance';
+                    routeDistanceDiv.textContent = `${distance.toFixed(2)} km`;
+                    popupDiv.appendChild(routeDistanceDiv);
+
+                    // Delete button
+                    const deleteButton = document.createElement('button');
+                    deleteButton.className = 'my-routes-delete-btn';
+                    deleteButton.textContent = 'Delete Route';
+                    deleteButton.onclick = function () {
+                        if (window.confirm("Are you sure you want to delete this route?")) {
+                            deleteRoute(route._id);
+                        }
+                    };
+                    popupDiv.appendChild(deleteButton);
+
+                    //------------------------------------------------------->
+
+                    // Add popup to map
+                    new mapboxgl.Popup({
+                        closeOnClick: false,
+                        closeButton: false,
+                        className: 'route-popup'
+                    })
+                        .setLngLat(route.coordinates[0])
+                        .setDOMContent(popupDiv)
+                        .addTo(map);
+                });
+            });
+
+            // Clean up map instance on component unmount
+            return () => map.remove();
+        };
+
+        // Initialize the map
+        initializeMap();
     }, [routes]);
 
+    /*
+     * Event handlers
+     */
+    const handleLogoClick = () => { navigate('/dashboard'); };
+    const handleBackClick = () => { navigate('/dashboard'); };
 
-
-    const handleLogoClick = () => {
-        navigate('/dashboard');
-    };
-
-    const handleBackClick = () => {
-        navigate('/dashboard');
-    };
-
+    /*
+     *Rendered JSX
+     */
     return (
         <div className="relativeContainer">
             {error && <div className="errorNotification">{error}</div>}
