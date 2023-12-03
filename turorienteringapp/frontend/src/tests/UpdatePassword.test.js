292951f1056bdbdce305dc5bc133f6aac0ab152b
ahmadmm <ahmadmm@stud.ntnu.no>
2023-12-03 16:42:30 +0100

    Add snapshot test for UpdatePassword component

--- turorienteringapp/frontend/src/tests/UpdatePassword.test.js
+++ turorienteringapp/frontend/src/tests/UpdatePassword.test.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 import UpdatePassword from '../components/UpdatePassword';
 
 test('UpdatePassword component renders without crashing', () => {
-  render(<UpdatePassword close={() => {}} />);
+  render(<UpdatePassword close={() => { }} />);
+});
+
+//---------------------------------------------->
+
+// Snapshot test for the update pasword component
+test('UpdatePassword component snapshot', () => {
+  const { asFragment } = render(
+    <BrowserRouter>
+      <UpdatePassword />
+    </BrowserRouter>
+  );
+  expect(asFragment()).toMatchSnapshot();
 });
