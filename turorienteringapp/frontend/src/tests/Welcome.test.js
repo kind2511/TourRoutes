9ec893331579ce4c2d654d959e08b62ca0d2c134
ahmadmm <ahmadmm@stud.ntnu.no>
2023-12-03 16:27:08 +0100

    Add snapshot test for Welcome component

--- turorienteringapp/frontend/src/tests/Welcome.test.js
+++ turorienteringapp/frontend/src/tests/Welcome.test.js
@@ -3,6 +3,7 @@ import { render } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import Welcome from '../components/Welcome';
 
+// Test to check if the Welcome component renders without crashing
 test('Welcome component renders without crashing', () => {
     render(
         <BrowserRouter>
@@ -10,3 +11,16 @@ test('Welcome component renders without crashing', () => {
         </BrowserRouter>
     );
 });
+
+//---------------------------------------------->
+
+// Snapshot test for the Welcome component
+test('Welcome component snapshot', () => {
+    const { asFragment } = render(
+      <BrowserRouter>
+        <Welcome />
+      </BrowserRouter>
+    );
+    expect(asFragment()).toMatchSnapshot();
+  });
+  
