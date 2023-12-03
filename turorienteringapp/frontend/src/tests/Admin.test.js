e0cc22965a3803d68262a3b0c19d4f5fae8d3bea
ahmadmm <ahmadmm@stud.ntnu.no>
2023-12-03 16:34:02 +0100

    Add snapshot test and update existing test for Admin component

--- turorienteringapp/frontend/src/tests/Admin.test.js
+++ turorienteringapp/frontend/src/tests/Admin.test.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import {BrowserRouter} from 'react-router-dom';
 import Admin from '../components/Admin';
 
 test('Admin component renders without crashing', () => {
   render(<Admin />);
+});
+
+//---------------------------------------->
+
+// Snapshot test for the Admin component
+test('Admin component snapshot', () => {
+  const { asFragment } = render(
+    <BrowserRouter>
+      <Admin />
+    </BrowserRouter>
+  );
+  expect(asFragment()).toMatchSnapshot();
 });
