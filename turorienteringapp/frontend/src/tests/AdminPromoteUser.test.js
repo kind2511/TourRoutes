6a8b3d948fbf1e83fe95a50296fc1c18e298ac89
ahmadmm <ahmadmm@stud.ntnu.no>
2023-12-03 19:22:21 +0100

    Add snapshot test for PromoteUserButton component

--- turorienteringapp/frontend/src/tests/AdminPromoteUser.test.js
+++ turorienteringapp/frontend/src/tests/AdminPromoteUser.test.js
@@ -5,3 +5,11 @@ import PromoteUserButton from '../components/AdminPromoteUser';
 test('PromoteUserButton renders without crashing', () => {
   render(<PromoteUserButton userId={1} onPromote={() => {}} />);
 });
+
+//---------------------------------->
+
+// Snapshot test for the PromoteUserButton component
+test('PromoteUserButton component snapshot', () => {
+  const { asFragment } = render(<PromoteUserButton userId={1} onPromote={() => {}} />);
+  expect(asFragment()).toMatchSnapshot();
+});
