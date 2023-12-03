cd7c570baadb10d16123b6555941ff8b5a4e2c92
ahmadmm <ahmadmm@stud.ntnu.no>
2023-12-03 19:06:30 +0100

    Add snapshot test for DeleteRouteButton component

--- turorienteringapp/frontend/src/tests/AdminDeleteRoute.test.js
+++ turorienteringapp/frontend/src/tests/AdminDeleteRoute.test.js
@@ -5,3 +5,11 @@ import DeleteRouteButton from '../components/AdminDeleteRoute';
 test('DeleteRouteButton renders without crashing', () => {
   render(<DeleteRouteButton routeId={1} onDelete={() => {}} />);
 });
+
+//--------------------------------------->
+
+// Snapshot test for the DeleteRouteButton 
+test('DeleteRouteButton component snapshot', () => {
+  const { asFragment } = render(<DeleteRouteButton routeId={1} onDelete={() => {}} />);
+  expect(asFragment()).toMatchSnapshot();
+});
