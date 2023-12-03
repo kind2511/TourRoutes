77314ec8e8eff4afefa89233f935b6ed5cad45bd
ahmadmm <ahmadmm@stud.ntnu.no>
2023-12-03 17:46:15 +0100

    Add snapshot test for logout component

--- turorienteringapp/frontend/src/tests/Logout.test.js
+++ turorienteringapp/frontend/src/tests/Logout.test.js
@@ -2,8 +2,9 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import Logout from '../components/Logout';
-import { AuthProvider } from '../components/AuthContext'; 
+import { AuthProvider } from '../components/AuthContext';
 
+// Test for rendering Logout component without crashing
 test('Logout component renders without crashing', () => {
   render(
     <BrowserRouter>
@@ -12,4 +13,18 @@ test('Logout component renders without crashing', () => {
       </AuthProvider>
     </BrowserRouter>
   );
+});
+
+//------------------------------------------------->
+
+// Snapshot test for the Logout component
+test('Logout component snapshot', () => {
+  const { asFragment } = render(
+    <BrowserRouter>
+      <AuthProvider>
+        <Logout />
+      </AuthProvider>
+    </BrowserRouter>
+  );
+  expect(asFragment()).toMatchSnapshot();
 });
