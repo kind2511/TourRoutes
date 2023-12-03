fb3869b1811b5dae3ff10f238cbd847d8e7fca99
ahmadmm <ahmadmm@stud.ntnu.no>
2023-12-03 19:13:41 +0100

    Add snapshot test for DeleteUserButton component

--- turorienteringapp/frontend/src/tests/AdminDeleteUser.test.js
+++ turorienteringapp/frontend/src/tests/AdminDeleteUser.test.js
@@ -5,3 +5,11 @@ import DeleteUserButton from '../components/AdminDeleteUser';
 test('DeleteUserButton renders without crashing', () => {
   render(<DeleteUserButton userId={1} onDelete={() => {}} />);
 });
+
+//------------------------------------------------->
+
+// Snapshot test for the DeleteUserButton component
+test('DeleteUserButton component snapshot', () => {
+  const { asFragment } = render(<DeleteUserButton userId={1} onDelete={() => {}} />);
+  expect(asFragment()).toMatchSnapshot();
+});
