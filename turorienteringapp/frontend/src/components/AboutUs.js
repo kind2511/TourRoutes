e0ca236b1ec214defd911455c26de238dccb984a
ahmadmm <ahmadmm@stud.ntnu.no>
2023-12-04 16:29:21 +0100

    Update About Us section

--- turorienteringapp/frontend/src/components/AboutUs.js
+++ turorienteringapp/frontend/src/components/AboutUs.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';  // Importing the useNavigate hook
 import './AboutUs.css';
 
-/* function to create segments*/ 
+
+/* function to create segments*/
 function AboutUs() {
     const text = {
         welcome: `About Us`,
@@ -12,9 +13,9 @@ function AboutUs() {
         share: `Our tool helps you find the quickest way between two places. Just tell it where to go, and it shows you the way.`,
         voice: `Our app is easy to use and looks nice. It's made for everyone to enjoy using.`,
         sustainable: `We keep all your data safe. You can trust us with your routes and information.`,
-        conclusion: `Our platfo is more than just maps. It's a tool for exploring the world easily to make your travel easy, efficient, and enjoyable.`
+        conclusion: `Our platform is more than just maps. It's a tool for exploring the world easily to make your travel easy, efficient, and enjoyable.`
     };
-    
+
     //Navigate Function
     const navigate = useNavigate();
 
@@ -24,15 +25,15 @@ function AboutUs() {
     };
 
     return (
-        <div className="container">
-            <div className="title">{text.welcome}</div>
-            <div className="segment">{text.precision}</div>
-            <div className="segment">{text.tailored}</div>
-            <div className="segment">{text.updates}</div>
-            <div className="segment">{text.share}</div>
-            <div className="segment">{text.voice}</div>
-            <div className="segment">{text.sustainable}</div>
-            <div className="conclusion">{text.conclusion}</div>
+        <div className="aboutus-container">
+            <div className="aboutus-title">{text.welcome}</div>
+            <div className="aboutus-segment">{text.precision}</div>
+            <div className="aboutus-segment">{text.tailored}</div>
+            <div className="aboutus-segment">{text.updates}</div>
+            <div className="aboutus-segment">{text.share}</div>
+            <div className="aboutus-segment">{text.voice}</div>
+            <div className="aboutus-segment">{text.sustainable}</div>
+            <div className="aboutus-conclusion">{text.conclusion}</div>
             <div className="aboutus-logo" onClick={handleLogoClick}>TurRuter</div>
         </div>
     );
